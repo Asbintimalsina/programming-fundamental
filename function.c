b72d651d4aaec126fa8c62c59841c108bc5251ef
Asbintimalsina <asbin.timalsina2054@gmail.com>
2017-01-18 11:30:10 +0545

    Add files via upload

---
+++ function.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void function (int num);
+int main()
+{
+
+void function1(int num)
+{
+		printf("the number is:%d",num);
+	
+}
+
+	int num;
+	printf("Enter the number\n");
+	scanf("%d",&num);
+	function1(num);
+return 0;
+}
+
+
+
+
+
+
